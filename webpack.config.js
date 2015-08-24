455348ce3a8b22096b3a662b2fbfb7539e62026e
Patrick Kelly <patrick@shopigniter.com>
2015-08-24 15:36:37 -0700

    webpack works

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  context: __dirname + "/app",
+  entry: {
+    javascript: "./app.js",
+    html: "./index.html"
+  },
+
+
+  output: {
+    filename: "app.js",
+    path: __dirname + "/dist"
+  },
+
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ["babel-loader"],
+      },
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]",
+      }
+    ],
+  }
+
+
+}
