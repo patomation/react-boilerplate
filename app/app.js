c992accfac6597f6053e35c1f3fe20becd07ed3b
Patrick Kelly <patrick@shopigniter.com>
2015-11-12 09:53:32 -0800

    feature/Updates to react 14 and adds google analytics

--- app/app.js
+++ app/app.js
@@ -1,16 +1,26 @@
 //Modules
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+//CSS
 import './main.scss'
 
 //Components
 import Layout from './components/layout'
 
+var App = React.createClass({
+  render: function() {
+    return(
+      <div className='app'>
+        <Layout>
+          Hello World
+        </Layout>
+      </div>
+    )
+  }
+});
 
-React.render(
-  <div className='app'>
-    <Layout>
-      Hello World
-    </Layout>
-  </div>,
-  document.body
+ReactDOM.render(
+  <App/>,
+  document.getElementById('App')
 );
