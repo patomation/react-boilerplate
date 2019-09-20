be47f7bf11fb9d091ff19463be7c59dd163c4be9
patomation <patomation@gmail.com>
2019-09-20 13:40:13 +0700

    Supports Hotloading

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import style from "./sass/main.scss";
 
 import App from './App.js';
 
+
+if (module && module.hot) {
+  module.hot.accept();
+}
+
+
 render(
   <App />,
   document.getElementById("root")
