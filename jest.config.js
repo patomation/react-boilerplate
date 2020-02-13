0d2edaae4733915cbd5c641ba1015c870a83998e
patomation <patomation@gmail.com>
2020-02-13 17:45:32 +0700

    feat: supports jest unit tests

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  verbose: true,
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  moduleDirectories: [
+    'node_modules', 'src'
+  ],
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'jsx',
+    'node'
+  ],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js'
+  },
+  roots: [
+    '<rootDir>/src'
+  ],
+  setupFiles: ['./setupTests.js'],
+  testEnvironment: 'node',
+  testPathIgnorePatterns: [
+    '\\\\node_modules\\\\'
+  ],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.js?$'
+}
