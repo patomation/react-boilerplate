da7440599dfe92bf8bf7aebf5322d54f3a79b901
Patrick Kelly <patomation@gmail.com>
2022-08-29 21:53:59 -0700

    chore(deps): parting ways with vurnerable node-sass and sass

--- jest.config.js
+++ jest.config.js
@@ -1,34 +1,22 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
   verbose: true,
   clearMocks: true,
-  coverageDirectory: 'coverage',
-  moduleDirectories: [
-    'node_modules', 'src'
-  ],
-  moduleFileExtensions: [
-    'js',
-    'json',
-    'jsx',
-    'ts',
-    'tsx',
-    'node'
-  ],
+  coverageDirectory: "coverage",
+  moduleDirectories: ["node_modules", "src"],
+  moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js'
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css)$": "<rootDir>/__mocks__/styleMock.js",
   },
-  roots: [
-    path.resolve('src')
-  ],
-  setupFiles: ['./jest.setup.js'],
-  testEnvironment: 'node',
-  testPathIgnorePatterns: [
-    '\\\\node_modules\\\\'
-  ],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  roots: [path.resolve("src")],
+  setupFiles: ["./jest.setup.js"],
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["\\\\node_modules\\\\"],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  }
-}
+    "^.+\\.tsx?$": "ts-jest",
+  },
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React from "react";
+import { render } from "react-dom";
 
-import './sass/main.scss'
-import './images/icons/favicon.ico'
+import "./main.css";
+import "./images/icons/favicon.ico";
 
-import App from './App'
+import App from "./App";
 
 if (module && module.hot) {
-  module.hot.accept()
+  module.hot.accept();
 }
 
 render(
-  <App message={'< ReactBoilerplate />'} />,
-  document.getElementById('root')
-)
+  <App message={"< ReactBoilerplate />"} />,
+  document.getElementById("root")
+);
