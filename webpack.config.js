ae1f9978f2d6cf4a9638dd8387906a0d4302e04c
Patrick Kelly <patomation@gmail.com>
2022-08-29 21:53:59 -0700

    chore(deps): parting ways with vurnerable node-sass and sass

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,6 @@ module.exports = {
         use: [
           "style-loader", // creates style nodes from JS strings
           "css-loader", // translates CSS into CommonJS
-          "sass-loader", // compiles Sass to CSS, using Node Sass by default
         ],
       },
       {
