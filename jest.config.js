be691a3d70937e416e3f85ad07bdc306397bd30e
Patrick <patomation@gmail.com>
2023-10-01 13:26:04 -0700

    feat(dev): support fetch in tests

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,37 @@
-const path = require("path");
+//@ts-check
+const path = require('path')
+const fetch = require('node-fetch')
 
 module.exports = {
   verbose: true,
   clearMocks: true,
-  coverageDirectory: "coverage",
-  moduleDirectories: ["node_modules", "src"],
-  moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"],
+  coverageDirectory: 'coverage',
+  moduleDirectories: ['node_modules', 'src'],
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'jsx',
+    'ts',
+    'tsx',
+    'node',
+  ],
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__mocks__/fileMock.js",
-    "\\.(css)$": "<rootDir>/__mocks__/styleMock.js",
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css)$':
+      '<rootDir>/__mocks__/styleMock.js',
   },
-  roots: [path.resolve("src")],
-  setupFiles: ["./jest.setup.js"],
-  testEnvironment: "node",
-  testPathIgnorePatterns: ["\\\\node_modules\\\\"],
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  roots: [path.resolve('src')],
+  setupFiles: ['./jest.setup.js'],
+  testEnvironment: 'node',
+  testPathIgnorePatterns: [
+    '\\\\node_modules\\\\',
+  ],
+  testRegex:
+    '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    '^.+\\.tsx?$': 'ts-jest',
   },
-};
+  // ReferenceError: fetch is not defined https://github.com/mswjs/msw/issues/686
+  globals: { fetch },
+}
