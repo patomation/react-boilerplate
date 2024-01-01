65707a2072bc59ace73d8689c12b8d1e322065f8
Patrick <patomation@gmail.com>
2024-01-01 12:36:16 -0800

    feat(react): using #app instead of #root to be closer to documentation

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,6 @@ import App from './App'
 if (module && module.hot) {
   module.hot.accept()
 }
-const container = document.getElementById('root')
+const container = document.getElementById('app')
 const root = createRoot(container)
 root.render(<App />)
