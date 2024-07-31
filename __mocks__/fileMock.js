83b8eec10ea69b859e33aff41ec5858be4556e21
Patrick <patomation@gmail.com>
2024-07-30 22:35:54 -0700

    feat: added jest fileMock

---
+++ __mocks__/fileMock.js
@@ -0,0 +1,7 @@
+module.exports = {
+  moduleNameMapper: {
+    '/.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$/':
+      '/home/dex/source/bisonmedicalmassage.com/__mocks__/fileMock.js',
+  },
+  resolver: undefined,
+}
