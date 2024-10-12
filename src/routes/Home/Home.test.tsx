755176aacb69aac78a11b48d51400aec676fa32c
Patrick <patomation@gmail.com>
2024-10-11 18:53:24 -0700

    feat(deps): updated everything. removed eslint and  App.test.tsx

--- src/routes/Home/Home.test.tsx
+++ src/routes/Home/Home.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import Home from './Home'
+import {
+  createMemoryRouter,
+  RouterProvider,
+} from 'react-router-dom'
 
 describe('<Home />', () => {
   it('renders', () => {
-    render(<Home />)
+    const router = createMemoryRouter([
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ])
+    render(<RouterProvider router={router} />)
   })
 })
