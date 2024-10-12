a1c6b8822841ab71fd0e6caccddb411a3caa2894
Patrick <patomation@gmail.com>
2024-10-11 19:39:37 -0700

    fix(package): added node shebang so it works in node_modules/.bin/create-react-app

--- scripts/create-react-app.js
+++ scripts/create-react-app.js
@@ -1,23 +1,56 @@
+#!/usr/bin/env node
 const fs = require('fs')
 const path = require('path')
 
 const displayTitle = () => {
-  console.log('                  React Boilerplate    ')
-  console.log('                                       ')
-  console.log('                        /\\            ')
-  console.log('                       /  \\           ')
-  console.log('                      /    \\          ')
-  console.log('                     /      \\         ')
-  console.log('                    /        \\        ')
-  console.log('                   /          \\       ')
-  console.log('                  /   < () >   \\      ')
-  console.log('                 /    < () >    \\     ')
-  console.log('                /     < () >     \\    ')
-  console.log('               /                  \\   ')
-  console.log('              /        }--{        \\  ')
-  console.log('             /______________________\\ ')
-  console.log('                                       ')
-  console.log('                                       ')
+  console.log(
+    '                  React Boilerplate    '
+  )
+  console.log(
+    '                                       '
+  )
+  console.log(
+    '                        /\\            '
+  )
+  console.log(
+    '                       /  \\           '
+  )
+  console.log(
+    '                      /    \\          '
+  )
+  console.log(
+    '                     /      \\         '
+  )
+  console.log(
+    '                    /        \\        '
+  )
+  console.log(
+    '                   /          \\       '
+  )
+  console.log(
+    '                  /   < () >   \\      '
+  )
+  console.log(
+    '                 /    < () >    \\     '
+  )
+  console.log(
+    '                /     < () >     \\    '
+  )
+  console.log(
+    '               /                  \\   '
+  )
+  console.log(
+    '              /        }--{        \\  '
+  )
+  console.log(
+    '             /______________________\\ '
+  )
+  console.log(
+    '                                       '
+  )
+  console.log(
+    '                                       '
+  )
 }
 
 const color = {
@@ -29,7 +62,7 @@ const color = {
   },
   reset: () => {
     console.log('\x1b[0m') // resets color back to terminal color
-  }
+  },
 }
 
 // Recursive folder copier
@@ -37,14 +70,17 @@ const recursiveCopy = (source, destination) => {
   // If is file, copy as is
   if (fs.lstatSync(source).isFile()) {
     fs.copyFileSync(source, destination)
-  // If is folder, recursive copy
+    // If is folder, recursive copy
   } else {
     // Make destination folder
     fs.mkdirSync(destination)
     // iterate over elements in source folder
-    fs.readdirSync(source).forEach(element => {
+    fs.readdirSync(source).forEach((element) => {
       // recursive copy each item and let it decide if it is a folder or not
-      recursiveCopy(path.join(source, element), path.join(destination, element))
+      recursiveCopy(
+        path.join(source, element),
+        path.join(destination, element)
+      )
     })
   }
 }
@@ -60,11 +96,20 @@ const run = (command) => {
   })
 }
 
-const hyphenate = (string) => string
-  .replace(/(^[A-Z])/, ([first]) => first.toLowerCase())
-  .replace(/([A-Z])/g, ([letter]) => `-${letter.toLowerCase()}`)
+const hyphenate = (string) =>
+  string
+    .replace(/(^[A-Z])/, ([first]) =>
+      first.toLowerCase()
+    )
+    .replace(
+      /([A-Z])/g,
+      ([letter]) => `-${letter.toLowerCase()}`
+    )
 
-const cammelCase = (string) => string.replace(/-([a-z])/g, letter => letter[1].toUpperCase())
+const cammelCase = (string) =>
+  string.replace(/-([a-z])/g, (letter) =>
+    letter[1].toUpperCase()
+  )
 
 const createReactApp = async () => {
   try {
@@ -85,7 +130,9 @@ const createReactApp = async () => {
     } else {
       // Exit early
       color.error()
-      console.log('Project directory already exists')
+      console.log(
+        'Project directory already exists'
+      )
       color.reset()
       process.exit()
     }
@@ -99,30 +146,59 @@ const createReactApp = async () => {
       'node_modules',
       'scripts',
       '.git',
-      'dist'
+      'dist',
     ]
     sourceFiles.forEach((element) => {
       // ignore exluded directories an files
       if (!exclude.includes(element)) {
-        const sourceElementPath = path.join(sourceDir, element)
-        const destinationElementPath = path.join(projectDir, element)
+        const sourceElementPath = path.join(
+          sourceDir,
+          element
+        )
+        const destinationElementPath = path.join(
+          projectDir,
+          element
+        )
         // Make new package.json
         if (element === 'package.json') {
           // Set project name as package name
-          const packageData = JSON.parse(fs.readFileSync(sourceElementPath))
+          const packageData = JSON.parse(
+            fs.readFileSync(sourceElementPath)
+          )
           packageData.name = projectName
           delete packageData.main
-          delete packageData.scripts['create-react-app']
+          delete packageData.scripts[
+            'create-react-app'
+          ]
           packageData.private = true
           packageData.version = '0.0.0'
-          fs.writeFileSync(destinationElementPath, JSON.stringify(packageData, null, 2), 'utf8')
-        } else if (element === 'webpack.config.js') {
+          fs.writeFileSync(
+            destinationElementPath,
+            JSON.stringify(packageData, null, 2),
+            'utf8'
+          )
+        } else if (
+          element === 'webpack.config.js'
+        ) {
           // Set project name as title
-          let webpackConfig = fs.readFileSync(sourceElementPath, 'utf8')
-          webpackConfig = webpackConfig.replace(/React Boilerplate/g, cammelCase(projectName))
-          fs.writeFileSync(destinationElementPath, webpackConfig, 'utf8')
+          let webpackConfig = fs.readFileSync(
+            sourceElementPath,
+            'utf8'
+          )
+          webpackConfig = webpackConfig.replace(
+            /React Boilerplate/g,
+            cammelCase(projectName)
+          )
+          fs.writeFileSync(
+            destinationElementPath,
+            webpackConfig,
+            'utf8'
+          )
         } else {
-          recursiveCopy(sourceElementPath, destinationElementPath)
+          recursiveCopy(
+            sourceElementPath,
+            destinationElementPath
+          )
         }
       }
     })
@@ -135,7 +211,9 @@ const createReactApp = async () => {
     // Add files
     await run('git add .')
     // Commit files
-    await run('git commit -m "Created React App from React Boilerplate"')
+    await run(
+      'git commit -m "Created React App from @patomation/create-react-app"'
+    )
     console.log('Done')
   } catch (error) {
     color.error()
